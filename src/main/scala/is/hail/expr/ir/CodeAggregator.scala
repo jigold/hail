666a5f14f10f58f55a0a5ac248201aa628fac7b8
Jackie Goldstein <jigold@broadinstitute.org>
2018-07-05 12:19:36 -0400

    wip

--- src/main/scala/is/hail/expr/ir/CodeAggregator.scala
+++ src/main/scala/is/hail/expr/ir/CodeAggregator.scala
@@ -20,8 +20,7 @@ abstract class BaseCodeAggregator[Agg <: RegionValueAggregator : ClassTag : Type
 
   def seqOp(region: Code[Region], rva: Code[RegionValueAggregator], vs: Array[Code[_]], ms: Array[Code[Boolean]]): Code[Unit]
 
-  def toKeyedAggregator(keyType: Type): KeyedCodeAggregator[Agg] =
-    KeyedCodeAggregator[Agg](keyType, TDict(keyType, out), initOpArgTypes, seqOpArgTypes)
+  def toKeyedAggregator(keyType: Type): KeyedCodeAggregator[Agg]
 }
 
 /**
@@ -55,10 +54,13 @@ case class CodeAggregator[Agg <: RegionValueAggregator : ClassTag : TypeInfo](
       Code._throw(Code.newInstance[RuntimeException, String]("Aggregators must have non missing constructor arguments")),
       Code.newInstance[Agg](constrArgTypes, v))
   }
+
+  def toKeyedAggregator(keyType: Type): KeyedCodeAggregator[Agg] =
+    KeyedCodeAggregator[Agg](Array(keyType), TDict(keyType, out), initOpArgTypes, seqOpArgTypes)
 }
 
 case class KeyedCodeAggregator[Agg <: RegionValueAggregator : ClassTag : TypeInfo](
-  key: Type,
+  keys: Array[Type],
   out: Type,
   initOpArgTypes: Option[Array[Class[_]]] = None,
   seqOpArgTypes: Array[Class[_]] = Array.empty[Class[_]]) extends BaseCodeAggregator[Agg] {
@@ -71,12 +73,8 @@ case class KeyedCodeAggregator[Agg <: RegionValueAggregator : ClassTag : TypeInf
     krvAgg.invoke[Agg]("rvAgg").invoke("initOp", argTypes, args)(classTag[Unit])
   }
 
-  def seqOp(region: Code[Region], krva: Code[RegionValueAggregator], vs: Array[Code[_]], ms: Array[Code[Boolean]]): Code[Unit] = {
-    assert(vs.length == ms.length)
-
-    val krvAgg = Code.checkcast[KeyedRegionValueAggregator[Agg]](krva)
-
-    def wrapArg(arg: Code[_]): Code[_] = key match {
+  def getRVAgg(region: Code[Region], krva: Code[KeyedRegionValueAggregator[Agg]], keys: Array[Type], vs: Array[Code[_]], ms: Array[Code[Boolean]]): Code[RegionValueAggregator] = {
+    def wrapArg(arg: Code[_]): Code[_] = keys.head match {
       case _: TBoolean => Code.boxBoolean(coerce[Boolean](arg))
       case _: TInt32 | _: TCall => Code.boxInt(coerce[Int](arg))
       case _: TInt64 => Code.boxLong(coerce[Long](arg))
@@ -89,20 +87,41 @@ case class KeyedCodeAggregator[Agg <: RegionValueAggregator : ClassTag : TypeInf
       case _ =>
         Code.invokeScalaObject[Type, Region, Long, Any](
           UnsafeRow.getClass, "read",
-          krvAgg.invoke[Type]("keyType"), region, coerce[Long](arg))
+          krva.invoke[Type]("keyType"), region, coerce[Long](arg))
     }
 
     val wrappedKey = ms.head.mux(Code._null[Any], wrapArg(vs.head))
-    val m = krvAgg.invoke[mutable.Map[Any, Agg]]("m")
+    val m = krva.invoke[mutable.Map[Any, Agg]]("m")
 
-    val rva = Code(m.invoke[Any, Boolean]("contains", wrappedKey).mux(
-      Code._empty,
-      m.invoke[Any, Any, Unit]("update", wrappedKey, Code.checkcast[Agg](krvAgg.invoke[RegionValueAggregator]("rvAgg")).invoke[Agg]("copy"))),
-      m.invoke("apply", wrappedKey))
+    if (keys.length == 1) {
+      Code.checkcast[Agg](Code(m.invoke[Any, Boolean]("contains", wrappedKey).mux(
+        Code._empty,
+        m.invoke[Any, Any, Unit]("update", wrappedKey, Code.checkcast[Agg](krva.invoke[RegionValueAggregator]("rvAgg")).invoke[Agg]("copy"))),
+        m.invoke("apply", wrappedKey)))
+    } else {
+//      ???
+      val newkrva = Code.checkcast[KeyedRegionValueAggregator[Agg]](Code(m.invoke[Any, Boolean]("contains", wrappedKey).mux(
+        Code._empty,
+        m.invoke[Any, Any, Unit]("update", wrappedKey, Code.checkcast[KeyedRegionValueAggregator[Agg]](krva.invoke[RegionValueAggregator]("rvAgg")).invoke[KeyedRegionValueAggregator[Agg]]("copy"))),
+        m.invoke("apply", wrappedKey)))
+      getRVAgg(region, newkrva, keys.drop(1), vs.drop(1), ms.drop(1))
+    }
+  }
+
+  def seqOp(region: Code[Region], krva: Code[RegionValueAggregator], vs: Array[Code[_]], ms: Array[Code[Boolean]]): Code[Unit] = {
+    assert(vs.length == ms.length)
+
+    val krvAgg = Code.checkcast[KeyedRegionValueAggregator[Agg]](krva)
+    val rva = getRVAgg(region, krvAgg, keys, vs, ms)
+
+    val nKeys = keys.length
 
     val argTypes = classOf[Region] +: seqOpArgTypes.flatMap[Class[_], Array[Class[_]]](Array(_, classOf[Boolean]))
-    val args = vs.drop(1).zip(ms.drop(1)).flatMap { case (v, m) => Array(v, m) }
+    val args = vs.drop(nKeys).zip(ms.drop(nKeys)).flatMap { case (v, m) => Array(v, m) }
 
-    rva.invoke("seqOp", argTypes, Array(region) ++ args)(classTag[Unit])
+    Code.checkcast[Agg](rva).invoke("seqOp", argTypes, Array(region) ++ args)(classTag[Unit])
   }
+
+  def toKeyedAggregator(keyType: Type): KeyedCodeAggregator[Agg] =
+    KeyedCodeAggregator[Agg](keys ++ Array(keyType), TDict(keyType, out), initOpArgTypes, seqOpArgTypes)
 }
