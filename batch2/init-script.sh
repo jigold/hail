93b20959867a72d8651549ce2bb85578b777ee29
Jackie Goldstein <jigold@broadinstitute.org>
2019-07-29 11:43:40 -0400

    wip

--- batch2/init-script.sh
+++ batch2/init-script.sh
@@ -1,3 +1,25 @@
 
 sudo apt update
+
 sudo apt install python3-pip
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get install docker-ce
+
+sudo groupadd docker # fails bc already exists
+
+sudo usermod -aG docker $USER
+
+sudo su - $USER
