5aed99442cfb2572833600ca0e79e5baefd49144
Jackie Goldstein <jigold@broadinstitute.org>
2019-08-02 11:39:41 -0400

    wip

--- batch2/batch/batch.py
+++ batch2/batch/batch.py
@@ -11,9 +11,11 @@ from .utils import abort, jsonify
 app = web.Application(client_max_size=None)
 routes = web.RouteTableDef()
 
+
 @routes.get('/healthcheck')
 async def get_healthcheck(request):  # pylint: disable=W0613
     return jsonify({})
+
 
 app.add_routes(routes)
 web.run_app(app, host='0.0.0.0', port=5000)
