3bd4ed207ccf0eed17d7f9b6c51595cb350c8d1f
Jackie Goldstein <jigold@broadinstitute.org>
2019-08-09 17:11:58 -0400

    wip

---
+++ batch2/batch/driver.py
@@ -0,0 +1,24 @@
+import asyncio
+
+
+class Driver:
+    # needs an event queue
+    # needs a scheduler instance
+    
+    def create_pod(self, body):
+        # get free instance
+        # submit request to that instance
+        # update db
+        pass
+
+    def delete_pod(self, name):
+        pass
+
+    def read_pod_log(self, name, container):
+        pass
+
+    def read_pod_status(self, name):
+        pass
+
+    def list_pods(self):
+        pass
