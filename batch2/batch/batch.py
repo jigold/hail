ab8e00072f8fe295c9f29341a2cb5d4f4e42d3ae
Jackie Goldstein <jigold@broadinstitute.org>
2019-08-11 16:10:15 -0400

    wip

--- batch2/batch/batch.py
+++ batch2/batch/batch.py
@@ -1301,7 +1301,7 @@ async def on_startup(app):
     app['driver'] = None
     app['log_store'] = LogStore(pool, INSTANCE_ID)
 
-    asyncio.ensure_future(polling_event_loop())
+    # asyncio.ensure_future(polling_event_loop())
     # asyncio.ensure_future(kube_event_loop())
     # asyncio.ensure_future(db_cleanup_event_loop())
 
