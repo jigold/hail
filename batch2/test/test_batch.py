ac94d87f256d2987ed211a865e0908d04acb69d1
Jackie Goldstein <jigold@broadinstitute.org>
2019-08-15 17:12:31 -0400

    wip

--- batch2/test/test_batch.py
+++ batch2/test/test_batch.py
@@ -40,7 +40,7 @@ class Test(unittest.TestCase):
     def tearDown(self):
         self.client.close()
 
-    def test_aaaa_garbage_image(self):
+    def test_garbage_image(self):
         builder = self.client.create_batch()
         j = builder.create_job('dsafaaadsf', ['echo', 'test'])
         builder.submit()
