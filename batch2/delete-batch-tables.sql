8a4ac41e21d17a95dd1b49c21a84d8b79d95946e
Jackie Goldstein <jigold@broadinstitute.org>
2019-09-03 16:06:30 -0400

    wip

--- batch2/delete-batch-tables.sql
+++ batch2/delete-batch-tables.sql
@@ -2,3 +2,5 @@ DROP TABLE IF EXISTS `jobs`;
 DROP TABLE IF EXISTS `batch-attributes`;
 DROP TABLE IF EXISTS `jobs-parents`;
 DROP TABLE IF EXISTS `batch`;
+DROP TABLE IF EXISTS `pods`;
+DROP TABLE IF EXISTS `instances`;
