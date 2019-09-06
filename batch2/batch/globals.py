d8dd2abebb42ab30c04e6b6c76a34d9e830cd40a
Jackie Goldstein <jigold@broadinstitute.org>
2019-09-06 14:35:42 -0400

    wip

--- batch2/batch/globals.py
+++ batch2/batch/globals.py
@@ -20,9 +20,6 @@ valid_state_transitions = {
 tasks = ('setup', 'main', 'cleanup')
 
 
-db = None
-
-
 def init_db():
     global db
     db = BatchDatabase.create_synchronous('/batch-user-secret/sql-config.json')
