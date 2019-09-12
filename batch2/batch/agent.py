cb40494b6254c7e6cc55da8edb596d4d5a2d5750
Jackie Goldstein <jigold@broadinstitute.org>
2019-09-12 17:00:25 -0400

    wip

--- batch2/batch/agent.py
+++ batch2/batch/agent.py
@@ -494,7 +494,7 @@ class Worker:
                         'ip_address': self.ip_address}
                 async with aiohttp.ClientSession(
                         raise_for_status=True, timeout=aiohttp.ClientTimeout(total=60)) as session:
-                    url = self.deploy_config.url('batch2', '/api/v1alpha/instances/activate')
+                    url = self.deploy_config.('batch2', '/api/v1alpha/instances/activate')
                     log.info(url)
                     async with session.post(url, json=body) as resp:
                         if resp.status == 200:
@@ -524,7 +524,7 @@ config = {
     'service_namespace': {}
 }
 
-deploy_config = get_deploy_config(_config=config)  # FIXME
+deploy_config = DeployConfig.from_config(config)  # FIXME
 worker = Worker(image, cores, deploy_config, inst_token, ip_address)
 
 loop = asyncio.get_event_loop()
