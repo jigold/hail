e09546c3555d95122209a462840ff8267f30c67c
Jackie Goldstein <jigold@broadinstitute.org>
2019-09-12 16:27:30 -0400

    wip

--- batch2/test/test_dag.py
+++ batch2/test/test_dag.py
@@ -5,8 +5,6 @@ import aiohttp
 import re
 from flask import Response
 
-import hailtop.gear.auth as hj
-
 from hailtop.batch_client.client import BatchClient, Job
 import hailtop.batch_client.aioclient as aioclient
 from hailtop.gear.auth import get_userinfo
