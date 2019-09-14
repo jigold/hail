6577a173bdab8c11a21f4516bc96b318216b4080
Jackie Goldstein <jigold@broadinstitute.org>
2019-09-14 16:42:17 -0400

    wip

--- batch2/batch/driver.py
+++ batch2/batch/driver.py
@@ -301,6 +301,7 @@ class Driver:
         if not inst:
             log.warning(f'pod_complete from unknown inst {inst_token}')
             raise web.HTTPNotFound()
+        inst.update_timestamp()
 
         pod_name = status['metadata']['name']
         pod = self.pods.get(pod_name)
