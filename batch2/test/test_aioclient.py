3d54a8c0ed2cdf3bba9f121dfd24a25e6e7a49e1
Jackie Goldstein <jigold@broadinstitute.org>
2019-09-15 11:22:52 -0400

    wip

--- batch2/test/test_aioclient.py
+++ batch2/test/test_aioclient.py
@@ -1,8 +1,10 @@
 import os
 import asyncio
+import pytest
 import aiohttp
 import unittest
 from hailtop.batch_client.aioclient import BatchClient
+
 
 
 def async_to_blocking(coro):
@@ -17,18 +19,18 @@ class Test(unittest.TestCase):
         loop = asyncio.get_event_loop()
         loop.run_until_complete(self.client.close())
 
-    def test_job(self):
-        async def f():
-            b = self.client.create_batch()
-            j = b.create_job('alpine', ['echo', 'test'])
-            await b.submit()
-            status = await j.wait()
-            self.assertTrue('attributes' not in status, (status, await j.log()))
-            self.assertEqual(status['state'], 'Success', (status, await j.log()))
-            self.assertEqual(status['exit_code']['main'], 0, (status, await j.log()))
+    @pytest.mark.asyncio
+    async def test_job(self):
+        b = self.client.create_batch()
+        j = b.create_job('alpine', ['echo', 'test'])
+        await b.submit()
+        status = await j.wait()
+        self.assertTrue('attributes' not in status, (status, await j.log()))
+        self.assertEqual(status['state'], 'Success', (status, await j.log()))
+        self.assertEqual(status['exit_code']['main'], 0, (status, await j.log()))
 
-            self.assertEqual((await j.log())['main'], 'test\n')
+        self.assertEqual((await j.log())['main'], 'test\n')
 
-            self.assertTrue(await j.is_complete())
+        self.assertTrue(await j.is_complete())
 
-        async_to_blocking(f())
+        # async_to_blocking(f())
