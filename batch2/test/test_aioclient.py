e41a03fcab0868672e7296d6760f80172681d0ff
Jackie Goldstein <jigold@broadinstitute.org>
2019-09-15 11:50:26 -0400

    wip

--- batch2/test/test_aioclient.py
+++ batch2/test/test_aioclient.py
@@ -13,26 +13,46 @@ from hailtop.batch_client.aioclient import BatchClient
 pytestmark = pytest.mark.asyncio
 
 
-class Test(unittest.TestCase):
-    async def setUp(self):
-        self.client = await BatchClient()
+@pytest.fixture(scope="module")
+async def batch_client():
+    bc = await BatchClient()
+    yield bc
+    await bc.close()
 
-    async def tearDown(self):
-        await self.client.close()
-        # loop = asyncio.get_event_loop()
-        # loop.run_until_complete(self.client.close())
 
-    async def test_job(self):
-        b = self.client.create_batch()
-        j = b.create_job('alpine', ['echo', 'test'])
-        await b.submit()
-        status = await j.wait()
-        self.assertTrue('attributes' not in status, (status, await j.log()))
-        self.assertEqual(status['state'], 'Success', (status, await j.log()))
-        self.assertEqual(status['exit_code']['main'], 0, (status, await j.log()))
+async def test_job(bc):
+    j = bc.create_job('alpine', ['echo', 'test'])
+    await bc.submit()
+    status = await j.wait()
+    assert 'attributes' not in status, (status, await j.log())
+    assert status['state'] == 'Success', (status, await j.log())
+    assert status['exit_code']['main'] == 0, (status, await j.log())
 
-        self.assertEqual((await j.log())['main'], 'test\n')
+    assert await j.log()['main'] == 'test\n'
 
-        self.assertTrue(await j.is_complete())
+    assert await j.is_complete()
 
-        # async_to_blocking(f())
+
+# class Test(unittest.TestCase):
+#     async def setUp(self):
+#         self.client = await BatchClient()
+#
+#     async def tearDown(self):
+#         await self.client.close()
+#         # loop = asyncio.get_event_loop()
+#         # loop.run_until_complete(self.client.close())
+#
+#     async def test_job(self):
+#         b = self.client.create_batch()
+#         j = b.create_job('alpine', ['echo', 'test'])
+#         await b.submit()
+#         status = await j.wait()
+#         self.assertTrue('attributes' not in status, (status, await j.log()))
+#         self.assertEqual(status['state'], 'Success', (status, await j.log()))
+#         self.assertEqual(status['exit_code']['main'], 0, (status, await j.log()))
+#
+#         self.assertEqual((await j.log())['main'], 'test\n')
+#
+#         self.assertTrue(await j.is_complete())
+#
+#         # async_to_blocking(f())
