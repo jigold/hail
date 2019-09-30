930486cc104d3789d0970599a20b55fb6d8325c8
Jackie Goldstein <jigold@broadinstitute.org>
2019-09-30 15:25:11 -0400

    wip

--- hail/python/hailtop/batch_client/aioclient.py
+++ hail/python/hailtop/batch_client/aioclient.py
@@ -9,7 +9,7 @@ from hailtop.auth import async_get_userinfo, service_auth_headers
 
 from .globals import complete_states
 
-job_array_size = 100
+job_array_size = 1000
 
 
 request_sem = asyncio.Semaphore(10)
