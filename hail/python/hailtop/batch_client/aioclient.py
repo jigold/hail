22b1a39016b8245f20fb06cdd2eec6888b0fa9cf
Jackie Goldstein <jigold@broadinstitute.org>
2019-10-04 14:38:41 -0400

    wip

--- hail/python/hailtop/batch_client/aioclient.py
+++ hail/python/hailtop/batch_client/aioclient.py
@@ -31,11 +31,11 @@ def filter_params(complete, success, attributes):
     return params
 
 
-async def retry(f, *args, **kwargs):
+async def retry(f):
     i = 0
     while True:
         try:
-            return await f(*args, **kwargs)
+            return await f()
         except Exception:  # pylint: disable=W0703
             j = random.randrange(math.floor(1.1 ** i))
             await asyncio.sleep(0.100 * j)
@@ -382,7 +382,7 @@ class BatchBuilder:
         return j
 
     async def _submit_job_with_retry(self, batch_id, docs):
-        return await retry(lambda *args, **kwargs: self._client._post(
+        return await retry(lambda: self._client._post(
             f'/api/v1alpha/batches/{batch_id}/jobs/create',
             json={'jobs': docs}
         ))
@@ -410,7 +410,6 @@ class BatchBuilder:
 
         batch = None
         try:
-            # b = await retry(self._client._post, '/api/v1alpha/batches/create', json=batch_doc)
             b = await self._client._post('/api/v1alpha/batches/create', json=batch_doc)
             batch = Batch(self._client, b['id'], b.get('attributes'))
 
@@ -429,7 +428,8 @@ class BatchBuilder:
 
             await self.pool.wait()
 
-            await self._client._patch(f'/api/v1alpha/batches/{batch.id}/close')  # FIXME: this needs a retry!
+            await retry(lambda: self._client.patch(f'/api/v1alpha/batches/{batch.id}/close'))
+            # await self._client._patch(f'/api/v1alpha/batches/{batch.id}/close')  # FIXME: this needs a retry!
         except Exception as err:  # pylint: disable=W0703
             if batch:
                 await batch.cancel()
