b7b4ecb5f685d16722ffacb69d699ad613c61ff7
Jackie Goldstein <jigold@broadinstitute.org>
2019-10-25 14:38:41 -0400

    wip

--- batch2/batch/batch.py
+++ batch2/batch/batch.py
@@ -427,6 +427,8 @@ class Batch:
         self.closed = closed
 
     async def get_jobs(self, limit=None, offset=None, size=None):
+        log.info(type(self.app['db'].jobs.get_records_by_batch(
+            self.id, limit=limit, offset=offset, size=size)))
         async for record in self.app['db'].jobs.get_records_by_batch(
                 self.id, limit=limit, offset=offset, size=size):
             yield Job.from_record(self.app, record)
@@ -445,6 +447,7 @@ class Batch:
 
     # called by driver
     async def _close_jobs(self):
+        log.info(type(self.get_jobs(size=1000)))
         async for job in self.get_jobs(size=1000):
             if job._state == 'Running':
                 await job._create_pod()
