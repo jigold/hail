3f0b47097a0e0aced129cc4d973e21e8402529d8
Jackie Goldstein <jigold@broadinstitute.org>
2020-01-07 16:35:26 -0500

    wip

--- batch/sql/create-billing-projects.sql
+++ batch/sql/create-billing-projects.sql
@@ -4,14 +4,8 @@ VALUES ('ci');
 INSERT INTO `billing_projects` (`name`)
 VALUES ('test');
 
-INSERT INTO `billing_projects` (`name`)
-VALUES ('hail');
-
 INSERT INTO `billing_project_users` (`billing_project`, `user`)
 VALUES ('ci', 'ci');
 
 INSERT INTO `billing_project_users` (`billing_project`, `user`)
 VALUES ('test', 'test');
-
-INSERT INTO `billing_project_users` (`billing_project`, `user`)
-VALUES ('hail', 'jigold');
