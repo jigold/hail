9ba1da3d2fe6d5782d06d6e6d34bf46f9dbebd4f
Jackie Goldstein <jigold@broadinstitute.org>
2020-01-11 09:15:31 -0500

    scale test setup

--- batch/batch/worker.py
+++ batch/batch/worker.py
@@ -275,12 +275,12 @@ class Container:
                         asyncio.ensure_future(worker.post_job_started(self.job))
 
                     async with self.step('starting'):
-                        await asyncio.sleep(0.1)
+                        await asyncio.sleep(5)
                         pass
                         # await docker_call_retry(self.container.start)
 
                     async with self.step('running'):
-                        await asyncio.sleep(0.1)
+                        await asyncio.sleep(5)
                         pass
                         # await docker_call_retry(self.container.wait)
 
