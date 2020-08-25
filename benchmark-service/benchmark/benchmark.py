e81c0442c1bf625a682fb227ca57c536a67a4fcc
Dania Abuhijleh <abuhijleh.d@northeastern.edu>
2020-08-25 10:55:34 -0400

    test

--- benchmark-service/benchmark/benchmark.py
+++ benchmark-service/benchmark/benchmark.py
@@ -157,7 +157,7 @@ async def show_name(request: web.Request, userdata) -> web.Response:  # pylint: 
 
 @router.get('/')
 @router.get('')
-@web_authenticated_developers_only(redirect=False)
+# @web_authenticated_developers_only(redirect=False)
 async def index(request, userdata):  # pylint: disable=unused-argument
     file = request.query.get('file')
     if file is None:
