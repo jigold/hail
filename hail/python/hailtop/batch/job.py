c15572d6eaf4341f3b414e7dad5ae36c512c19f6
Jackie Goldstein <jigold@broadinstitute.org>
2021-02-23 17:33:28 -0500

    docs

--- hail/python/hailtop/batch/job.py
+++ hail/python/hailtop/batch/job.py
@@ -396,6 +396,11 @@ class Job:
         *G*, *Gi*, *T*, *Ti*, *P*, and *Pi*. Omitting a suffix means
         the value is in bytes.
 
+        For the :class:`.ServiceBackend`, the values 'lowmem', 'standard',
+        and 'highmem' are also valid arguments. 'lowmem' corresponds to
+        approximately 1 Gi/core, 'standard' corresponds to approximately
+        4 Gi/core, and 'highmem' corresponds to approximately 7 Gi/core.
+
         Parameters
         ----------
         memory:
