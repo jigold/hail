4d7847a16f1b6390689e6105fd1deb24c3c0e310
Jackie Goldstein <jigold@broadinstitute.org>
2021-03-10 10:43:01 -0500

    add logging and sleep

--- batch/batch/worker/disk.py
+++ batch/batch/worker/disk.py
@@ -41,7 +41,6 @@ class Disk:
         await self._delete()
 
     async def _format(self):
-        await check_shell(f'df -hT; ls /dev/disk/by-id/')
         await asyncio.sleep(10)
         await check_shell(f'df -hT; ls /dev/disk/by-id/')
         await check_shell(f'mkfs.ext4 -m 0 -E lazy_itable_init=0,lazy_journal_init=0,discard {self.disk_path}', echo=True)
