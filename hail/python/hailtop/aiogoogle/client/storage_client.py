aeba83f063da8888698734a649da74c1ce0e591c
Jackie Goldstein <jigold@broadinstitute.org>
2021-03-25 14:06:40 -0400

    fix?

--- hail/python/hailtop/aiogoogle/client/storage_client.py
+++ hail/python/hailtop/aiogoogle/client/storage_client.py
@@ -659,6 +659,7 @@ class GoogleStorageAsyncFS(AsyncFS):
                         size = await status.size()
                         if size != 0:
                             raise FileAndDirectoryError(url)
+                        continue
                     yield GoogleStorageFileListEntry(url, item, status)
 
     async def _listfiles_flat(self, bucket: str, name: str) -> AsyncIterator[FileListEntry]:
@@ -686,6 +687,7 @@ class GoogleStorageAsyncFS(AsyncFS):
                         size = await status.size()
                         if size != 0:
                             raise FileAndDirectoryError(url)
+                        continue
                     yield GoogleStorageFileListEntry(url, item, status)
 
     async def listfiles(self, url: str, recursive: bool = False) -> AsyncIterator[FileListEntry]:
