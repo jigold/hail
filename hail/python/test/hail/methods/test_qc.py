174b0ba934d2c9013532fec7bb0a342019ef25bc
Jackie Goldstein <jigold@broadinstitute.org>
2021-04-20 21:24:06 -0400

    [qob] VEP for GRCh37

--- hail/python/test/hail/methods/test_qc.py
+++ hail/python/test/hail/methods/test_qc.py
@@ -279,3 +279,27 @@ class Tests(unittest.TestCase):
         assert r['n_variants'] == 346
         assert r['r_ti_tv'] == 2.5
         assert r['allele_counts'] == {2: 346}
+
+    @skip_unless_service_backend()
+    def test_vep_grch37(self):
+        assert False
+
+        # def get_test_variant_ann(mt):
+        #     mt_locus = mt.filter((mt.locus == hl.locus('1', 55518321)) &
+        #                          (mt.alleles == ['A', 'C']))
+        #     return mt_locus
+        #
+        # mt = hl.import_vcf(resource('vep_test_variant_grch37.vcf'))
+        # mt_ann = hl.vep(mt, csq=False, tolerate_parse_error=False)
+        # mt_locus = get_test_variant_ann(mt_ann)
+        # # assert mt_locus.vep.
+        # # assert mt_locus.globals
+        #
+        # mt_ann = hl.vep(mt, csq=False, tolerate_parse_error=True)
+        # mt_locus = get_test_variant_ann(mt_ann)
+        # mt_locus._force_count()
+        #
+        # mt_ann = hl.vep(mt, csq=True)
+        # mt_locus = get_test_variant_ann(mt_ann)
+        # # assert mt_locus.vep
+        # # assert mt_locus.globals
