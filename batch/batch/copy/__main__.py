9e7c1fcd9b2b4f41dd870d6f2301bf9c7cc50357
Jackie Goldstein <jigold@broadinstitute.org>
2021-05-26 16:42:17 -0400

    wip

--- batch/batch/copy/__main__.py
+++ batch/batch/copy/__main__.py
@@ -6,10 +6,6 @@ from concurrent.futures import ThreadPoolExecutor
 from hailtop.aiotools.fs import RouterAsyncFS, LocalAsyncFS, Transfer
 from hailtop.aiogoogle import GoogleStorageAsyncFS
 
-import tracemalloc
-
-tracemalloc.start()
-
 
 async def copy(requester_pays_project: Optional[str], transfer: Union[Transfer, List[Transfer]]) -> None:
     if requester_pays_project:
@@ -29,20 +25,9 @@ async def main() -> None:
     requster_pays_project = json.loads(sys.argv[1])
     files = json.loads(sys.argv[2])
 
-    try:
-        await copy(
-            requster_pays_project, [Transfer(f['from'], f['to'], treat_dest_as=Transfer.DEST_IS_TARGET) for f in files]
-        )
-    finally:
-        snapshot = tracemalloc.take_snapshot()
-        top_stats = snapshot.statistics('traceback')
-
-        # pick the biggest memory block
-        stat = top_stats[0]
-        print("%s memory blocks: %.1f KiB" % (stat.count, stat.size / 1024))
-        for line in stat.traceback.format():
-            print(line)
-        print(snapshot)
+    await copy(
+        requster_pays_project, [Transfer(f['from'], f['to'], treat_dest_as=Transfer.DEST_IS_TARGET) for f in files]
+    )
 
 
 if __name__ == '__main__':
