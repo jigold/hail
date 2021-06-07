e2350e3fa54423f2fdd3b1e80b75e6d06e9b0c8f
Jackie Goldstein <jigold@broadinstitute.org>
2021-06-07 10:19:54 -0400

    add memory monitor

--- batch/batch/copy/__main__.py
+++ batch/batch/copy/__main__.py
@@ -2,17 +2,40 @@ from typing import Union, List, Optional
 import sys
 import json
 import asyncio
+import resource
+import logging
+import humanize
 from concurrent.futures import ThreadPoolExecutor
 from hailtop.aiotools.fs import RouterAsyncFS, LocalAsyncFS, Transfer
 from hailtop.aiogoogle import GoogleStorageAsyncFS
 from hailtop.hail_logging import configure_logging
+from hailtop.aiotools import BackgroundTaskManager
 
 configure_logging()
+
+log = logging.getLogger('copy')
 
 # import tracemalloc
 #
 #
 # tracemalloc.start()
+
+
+class MemoryMonitor:
+    def __init__(self):
+        self.keep_measuring = True
+
+    async def measure_usage(self):
+        max_usage = 0
+        while self.keep_measuring:
+            max_usage = max(
+                max_usage,
+                resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+            )
+            log.info(f'max memory usage {humanize.naturalsize(max_usage * 1024, binary=True)}')
+            await asyncio.sleep(15)
+
+        return max_usage
 
 
 async def copy(requester_pays_project: Optional[str], transfer: Union[Transfer, List[Transfer]]) -> None:
@@ -29,16 +52,21 @@ async def copy(requester_pays_project: Optional[str], transfer: Union[Transfer, 
 
 
 async def main() -> None:
+    task_manager = BackgroundTaskManager()
+
     try:
         assert len(sys.argv) == 3
         requster_pays_project = json.loads(sys.argv[1])
         files = json.loads(sys.argv[2])
 
+        memory_monitor = MemoryMonitor()
+        task_manager.ensure_future(memory_monitor.measure_usage())
         await copy(
             requster_pays_project, [Transfer(f['from'], f['to'], treat_dest_as=Transfer.DEST_IS_TARGET) for f in files]
         )
     finally:
         print(sys.exc_info())
+        task_manager.shutdown()
 
 
 if __name__ == '__main__':
