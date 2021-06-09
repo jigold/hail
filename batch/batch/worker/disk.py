417b642f274588de0cd0777a2b9f3086d703d8ae
Jackie Goldstein <jigold@broadinstitute.org>
2021-06-09 13:40:31 -0400

    wip

--- batch/batch/worker/disk.py
+++ batch/batch/worker/disk.py
@@ -37,9 +37,15 @@ class Disk:
 
     async def delete(self):
         try:
-            await self._detach()
+            await self._unmount()
         finally:
-            await self._delete()
+            try:
+                await self._detach()
+            finally:
+                await self._delete()
+
+    async def _unmount(self):
+        await check_shell_output(f'umount {self.disk_path}')
 
     async def _format(self):
         await check_shell_output(f'mkfs.ext4 -m 0 -E lazy_itable_init=0,lazy_journal_init=0,discard {self.disk_path}')
