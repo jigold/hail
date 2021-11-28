33733cb7cfa36e0dce165769629d3fbdf08ed6a3
Jackie Goldstein <jigold@broadinstitute.org>
2021-11-28 13:05:43 -0500

    wip

--- batch/sql/add-latest-resource-versions.sql
+++ batch/sql/add-latest-resource-versions.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS `latest_resource_versions` (
   `prefix` VARCHAR(100) NOT NULL,
   `version` VARCHAR(100) NOT NULL,
-  PRIMARY KEY (`product`)
+  PRIMARY KEY (`prefix`)
 ) ENGINE = InnoDB;
