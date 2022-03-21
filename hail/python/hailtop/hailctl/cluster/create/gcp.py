8f97287684e448d2fa3460d77a6675481178ebbf
Jackie Goldstein <jigold@broadinstitute.org>
2022-03-21 11:15:22 -0400

    wip

--- hail/python/hailtop/hailctl/cluster/create/gcp.py
+++ hail/python/hailtop/hailctl/cluster/create/gcp.py
@@ -6,7 +6,7 @@ import tempfile
 from typing import List
 
 from hailtop import pip_version
-from hailtop.utils import CalledProcessError, async_to_blocking, filter_none, secret_alnum_string
+from hailtop.utils import CalledProcessError, async_to_blocking, filter_none, secret_alnum_string, check_shell_output, check_shell
 
 
 def init_parser(parser):
@@ -40,7 +40,7 @@ def init_parser(parser):
     commit.add_argument('--sha', type=str, help="Name of the SHA to create the driver from.", nargs='?')
 
 
-def main(args, pass_through_args):  # pylint: disable=unused-argument
+async def run(args, pass_through_args):  # pylint: disable=unused-argument
     runner_vm_name = f'mini-batch-{args.project}-runner'
     runner_sa_name = f'mini-batch-runner@{args.project}.iam.gserviceaccount.com'
     commit = (filter_none([args.branch, args.pip_version, args.sha]) + [pip_version()])[0]
@@ -50,12 +50,12 @@ def main(args, pass_through_args):  # pylint: disable=unused-argument
     if args.cluster_state_bucket is None:
         cluster_state_bucket = f'{args.project}-mini-batch-cluster-state'
         try:
-            os.system(f'gsutil ls gs://{cluster_state_bucket}')
-        except:
+            await check_shell_output(f'gsutil ls gs://{cluster_state_bucket}')
+        except CalledProcessError:
             print(f'bucket gs://{cluster_state_bucket} is not accessible. trying to create it.')
             try:
-                os.system(f'gsutil mb -p {args.project} -c {args.bucket_storage_class} -l {region} gs://{cluster_state_bucket}')
-            except:
+                await check_shell_output(f'gsutil mb -p {args.project} -c {args.bucket_storage_class} -l {region} gs://{cluster_state_bucket}')
+            except CalledProcessError:
                 print(f'error creating bucket gs://{cluster_state_bucket}')
                 raise
     else:
@@ -77,26 +77,35 @@ gcloud --project {args.project} services enable \
     artifactregistry.googleapis.com
 ''')
 
-    # FIXME: read oauth2 credentials in terraform?
     remote_oauth2_credentials_file = f'gs://{cluster_state_bucket}/mini-batch/oauth2/oauth2_credentials_file'
-    os.system(f'gsutil cp {shq(os.path.expanduser(args.oauth2_credentials_file))} {shq(remote_oauth2_credentials_file)}')
+    await check_shell_output(f'''
+gsutil cp {shq(os.path.expanduser(args.oauth2_credentials_file))} {shq(remote_oauth2_credentials_file)}
+''')
 
     with tempfile.TemporaryDirectory() as tmp:
         runner_script = f'{tmp}/runner.sh'
         with open(runner_script, 'w') as f:
             f.write(f'''
+sudo tee run.sh <<EOF
 #! /bin/bash
-set -ex
+set -x
 
+touch started
+gsutil cp started gs://${{TF_STATE_BUCKET}}/mini-batch/setup-driver/started
+
+NAME=$(curl -s -H 'Metadata-Flavor: Google' http://metadata.google.internal/computeMetadata/v1/instance/name)
+ZONE=$(curl -s -H 'Metadata-Flavor: Google' http://metadata.google.internal/computeMetadata/v1/instance/zone)
+PROJECT=$(curl -s -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/project/project-id")
 REPO=$(curl -s -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/attributes/repo")
 COMMIT=$(curl -s -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/attributes/commit")
+TF_STATE_BUCKET=$(curl -s -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/attributes/tf_state_bucket")
 
 sudo apt-get update
 sudo apt-get install -y git
 
-git clone https://github.com/${{REPO}}.git
+git clone https://github.com/$REPO.git
 cd hail/
-git checkout "${{COMMIT}}"
+git checkout "$COMMIT"
 cd /
 
 sudo apt-get update
@@ -119,21 +128,27 @@ sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(l
 sudo apt-get update
 sudo apt-get install terraform
 
-sudo tee /run.sh <<EOF
-sh /hail/infra/mini-batch/gcp/bootstrap.sh > bootstrap.log 2>&1
-touch /bootstrap-complete
-EOF 
+# Setup driver
+sh /hail/infra/mini-batch/gcp/setup.sh
 
-sudo nohup sh /run.sh > run.log 2>&1 &
+gsutil cp run.log gs://${{TF_STATE_BUCKET}}/mini-batch/setup-driver/log
+
+touch done
+gsutil cp done gs://${{TF_STATE_BUCKET}}/mini-batch/setup-driver/done
+
+gcloud --project $PROJECT -q compute instances delete $NAME --zone=$ZONE
+EOF
+
+sudo nohup /bin/bash run.sh >run.log 2>&1 & 
 ''')
 
         try:
-            os.system(f'''
+            await check_shell_output(f'''
 gcloud --project {args.project} iam service-accounts create {runner_sa_name} --display-name "{runner_sa_name}" && \
     gcloud --project {args.project} projects add-iam-policy-binding {args.project} --member='serviceAccount:{runner_sa_name}' --role='owner'
 ''')
 
-            os.system(f'''
+            await check_shell_output(f'''
 gcloud compute instances create {runner_vm_name} \
     --project {args.project} \
     --image-project ubuntu-os-cloud \
@@ -162,91 +177,38 @@ gcloud compute instances create {runner_vm_name} \
     --service-account {runner_sa_name}
 ''')
 
-            os.system(f'''
+            async def wait_for_file_exists(file):
+                while True:
+                    try:
+                        await check_shell(f'gsutil -q stat {file}')
+                    except CalledProcessError:
+                        pass
+                    else:
+                        return
 
+            await asyncio.wait_for(wait_for_file_exists(f'gs://${cluster_state_bucket}/mini-batch/setup-driver/started'), timeout=120)
+
+            complete_task = asyncio.wait_for(wait_for_file_exists(f'gs://${cluster_state_bucket}/mini-batch/setup-driver/started'), timeout=20 * 60)
+            stream_logs_task = check_shell_output(f'''
+gcloud --project {shq(args.project)} compute ssh {shq(runner_vm_name)} --command "sudo tail -f /run.log"
 ''')
+
+            try:
+                await asyncio.wait(complete_task, stream_logs_task, return_when=asyncio.FIRST_COMPLETED)
+                if complete_task.done():
+                    await complete_task.result()
+            finally:
+                for task in (complete_task, stream_logs_task):
+                    task.cancel()
         finally:
-            os.system(f'''
+            try:
+                await check_shell_output(f'''
+gcloud --project {shq(args.project)} -q compute instances delete {runner_vm_name} --zone={shq(args.zone)}
+''')
+            finally:
+                await check_shell_output(f'''
 gcloud --project {shq(args.project)} iam service-accounts delete {shq(runner_sa_name)}
 ''')
 
-# def main(args, pass_through_args):  # pylint: disable=unused-argument
-#     commit = (filter_none([args.branch, args.pip_version, args.sha]) + [pip_version()])[0]
-#
-#     region = args.zone.rsplit('-', maxsplit=1)[0]
-#
-#     if args.cluster_state_bucket is None:
-#         cluster_state_bucket = f'{args.project}-mini-batch-cluster-state'
-#         try:
-#             os.system(f'gsutil ls gs://{cluster_state_bucket}')
-#         except:
-#             print(f'bucket gs://{cluster_state_bucket} is not accessible. trying to create it.')
-#             try:
-#                 os.system(f'gsutil mb -p {args.project} -c {args.bucket_storage_class} -l {region} gs://{cluster_state_bucket}')
-#             except:
-#                 print(f'error creating bucket gs://{cluster_state_bucket}')
-#                 raise
-#     else:
-#         cluster_state_bucket = args.cluster_state_bucket
-#
-#     print(f'storing cluster state in gs://{cluster_state_bucket}')
-#
-#     print(f'enabling services')
-#     os.system(f'''
-# gcloud --project {args.project} services enable \
-#     container.googleapis.com \
-#     compute.googleapis.com \
-#     cloudresourcemanager.googleapis.com \
-#     servicenetworking.googleapis.com \
-#     sqladmin.googleapis.com \
-#     serviceusage.googleapis.com \
-#     logging.googleapis.com \
-#     iam.googleapis.com \
-#     artifactregistry.googleapis.com
-# ''')
-#
-#     # FIXME: read oauth2 credentials in terraform?
-#     remote_oauth2_credentials_file = f'gs://{cluster_state_bucket}/mini-batch/oauth2/oauth2_credentials_file'
-#     os.system(f'gsutil cp {shq(os.path.expanduser(args.oauth2_credentials_file))} {shq(remote_oauth2_credentials_file)}')
-#
-#     with tempfile.TemporaryDirectory() as tmp:
-#         print(f'initializing terraform files in {tmp}')
-#
-#         tf_dir = os.path.dirname(os.path.realpath(__file__)) + '/infra/gcp'
-#         print(tf_dir)
-#
-#         os.system(f'cp {tf_dir}/main.tf {tmp}/main.tf')
-#         os.system(f'cp {tf_dir}/variables.tf {tmp}/variables.tf')
-#         os.system(f'cp {tf_dir}/outputs.tf {tmp}/outputs.tf')
-#
-#         with open(f'{tmp}/inputs.tfvars', 'w') as f:
-#             f.write(f'''
-# cluster_name = "{args.project}"
-# gcp_project = "{args.project}"
-# gcp_region = "{region}"
-# gcp_zone = "{args.zone}"
-# gcp_location = "{region}"  # FIXME
-# organization_domain = "{args.organization_domain}"
-# username = "{args.username}"
-# email = "{args.email}"
-# n_cores = {args.cores}
-# boot_disk_size = {args.boot_disk_size}
-# machine_family = "{args.machine_family}"
-# machine_type = "{args.machine_type}"
-# # FIXME: db variables
-# bucket_storage_class = "{args.bucket_storage_class}"
-# bucket_location = "{region}"
-# oauth2_credentials_file = "{remote_oauth2_credentials_file}"
-# repo = "{args.repo}"
-# commit = "{commit}"
-# tf_state_bucket = "{cluster_state_bucket}"
-# ''')
-#
-#         os.system(f'''
-# set -ex
-# cd {tmp}
-# terraform init -backend-config "bucket={cluster_state_bucket}"
-# terraform apply -auto-approve -var-file inputs.tfvars
-# ''')
-#
-#     # SSH connect to runner to see progress???
+def main(args, pass_through_args):
+    async_to_blocking(run(args, pass_through_args))
