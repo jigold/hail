6eff716632b2b3e434b1ad09fe76cd38e2cb38de
Jackie Goldstein <jigold@broadinstitute.org>
2022-03-21 08:09:59 -0400

    wip

--- hail/python/hailtop/hailctl/cluster/create/gcp.py
+++ hail/python/hailtop/hailctl/cluster/create/gcp.py
@@ -41,6 +41,7 @@ def init_parser(parser):
 
 
 def main(args, pass_through_args):  # pylint: disable=unused-argument
+    runner_vm_name = f'mini-batch-{args.project}-runner'
     commit = (filter_none([args.branch, args.pip_version, args.sha]) + [pip_version()])[0]
 
     region = args.zone.rsplit('-', maxsplit=1)[0]
@@ -80,6 +81,60 @@ gcloud --project {args.project} services enable \
     os.system(f'gsutil cp {shq(os.path.expanduser(args.oauth2_credentials_file))} {shq(remote_oauth2_credentials_file)}')
 
     with tempfile.TemporaryDirectory() as tmp:
+        runner_script = f'{tmp}/runner.sh'
+        with open(runner_script, 'w') as f:
+            f.write(f'''
+#! /bin/bash
+set -ex
+
+sudo apt-get update
+sudo apt-get install -y git
+
+git clone https://github.com/{args.repo}.git
+cd hail/
+git checkout "{commit}"
+cd /
+
+sudo apt-get update
+
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    conntrack \
+    curl \
+    emacs-nox \
+    gnupg \
+    jq \
+    lsb-release \
+    net-tools \
+    software-properties-common
+
+# Install Terraform
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update
+sudo apt-get install terraform
+
+nohup sh /hail/infra/mini-batch/gcp/bootstrap.sh > bootstrap.log 2>&1 &
+''')
+
+        os.system(f'''
+gcloud compute instances create {runner_vm_name} \
+    --project {args.project} \
+    --image-project ... \
+    --boot-disk-size 30 \
+    --boot-disk-type pd-ssd \
+    --machine-type n1-standard-1 \
+    --metadata ... \
+    --metadata-from-file startup-script={runner_script} \
+    --no-restart-on-failure \
+    --tags mini-batch-runner \
+    --zone {args.zone} \
+    --scopes cloud-platform \
+    --service-account mini-batch-{args.project}-runner@{args.project}.
+    
+''')
+
         print(f'initializing terraform files in {tmp}')
 
         tf_dir = os.path.dirname(os.path.realpath(__file__)) + '/infra/gcp'
@@ -120,3 +175,85 @@ terraform apply -auto-approve -var-file inputs.tfvars
 ''')
 
     # SSH connect to runner to see progress???
+
+
+# def main(args, pass_through_args):  # pylint: disable=unused-argument
+#     commit = (filter_none([args.branch, args.pip_version, args.sha]) + [pip_version()])[0]
+#
+#     region = args.zone.rsplit('-', maxsplit=1)[0]
+#
+#     if args.cluster_state_bucket is None:
+#         cluster_state_bucket = f'{args.project}-mini-batch-cluster-state'
+#         try:
+#             os.system(f'gsutil ls gs://{cluster_state_bucket}')
+#         except:
+#             print(f'bucket gs://{cluster_state_bucket} is not accessible. trying to create it.')
+#             try:
+#                 os.system(f'gsutil mb -p {args.project} -c {args.bucket_storage_class} -l {region} gs://{cluster_state_bucket}')
+#             except:
+#                 print(f'error creating bucket gs://{cluster_state_bucket}')
+#                 raise
+#     else:
+#         cluster_state_bucket = args.cluster_state_bucket
+#
+#     print(f'storing cluster state in gs://{cluster_state_bucket}')
+#
+#     print(f'enabling services')
+#     os.system(f'''
+# gcloud --project {args.project} services enable \
+#     container.googleapis.com \
+#     compute.googleapis.com \
+#     cloudresourcemanager.googleapis.com \
+#     servicenetworking.googleapis.com \
+#     sqladmin.googleapis.com \
+#     serviceusage.googleapis.com \
+#     logging.googleapis.com \
+#     iam.googleapis.com \
+#     artifactregistry.googleapis.com
+# ''')
+#
+#     # FIXME: read oauth2 credentials in terraform?
+#     remote_oauth2_credentials_file = f'gs://{cluster_state_bucket}/mini-batch/oauth2/oauth2_credentials_file'
+#     os.system(f'gsutil cp {shq(os.path.expanduser(args.oauth2_credentials_file))} {shq(remote_oauth2_credentials_file)}')
+#
+#     with tempfile.TemporaryDirectory() as tmp:
+#         print(f'initializing terraform files in {tmp}')
+#
+#         tf_dir = os.path.dirname(os.path.realpath(__file__)) + '/infra/gcp'
+#         print(tf_dir)
+#
+#         os.system(f'cp {tf_dir}/main.tf {tmp}/main.tf')
+#         os.system(f'cp {tf_dir}/variables.tf {tmp}/variables.tf')
+#         os.system(f'cp {tf_dir}/outputs.tf {tmp}/outputs.tf')
+#
+#         with open(f'{tmp}/inputs.tfvars', 'w') as f:
+#             f.write(f'''
+# cluster_name = "{args.project}"
+# gcp_project = "{args.project}"
+# gcp_region = "{region}"
+# gcp_zone = "{args.zone}"
+# gcp_location = "{region}"  # FIXME
+# organization_domain = "{args.organization_domain}"
+# username = "{args.username}"
+# email = "{args.email}"
+# n_cores = {args.cores}
+# boot_disk_size = {args.boot_disk_size}
+# machine_family = "{args.machine_family}"
+# machine_type = "{args.machine_type}"
+# # FIXME: db variables
+# bucket_storage_class = "{args.bucket_storage_class}"
+# bucket_location = "{region}"
+# oauth2_credentials_file = "{remote_oauth2_credentials_file}"
+# repo = "{args.repo}"
+# commit = "{commit}"
+# tf_state_bucket = "{cluster_state_bucket}"
+# ''')
+#
+#         os.system(f'''
+# set -ex
+# cd {tmp}
+# terraform init -backend-config "bucket={cluster_state_bucket}"
+# terraform apply -auto-approve -var-file inputs.tfvars
+# ''')
+#
+#     # SSH connect to runner to see progress???
