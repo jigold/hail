b5fa84c56ea60223e3cb494c75b65bc8a9678691
Jackie Goldstein <jigold@broadinstitute.org>
2022-06-03 14:54:12 -0400

    more changes

--- batch/sql/populate_real_time_billing_tables.py
+++ batch/sql/populate_real_time_billing_tables.py
@@ -1,10 +1,11 @@
 import aiomysql
 import asyncio
+import tempfile
 import time
 import datetime
 
 from gear import Database, transaction
-from hailtop.utils import secret_alnum_string
+from hailtop.utils import check_shell, secret_alnum_string
 
 
 def get_resources_fmt_version_less_than_3(cores_mcpu):
@@ -16,6 +17,16 @@ def get_resources_fmt_version_less_than_3(cores_mcpu):
         ('ip-fee/1024/1', 64 * (cores_mcpu // 1000)),
         ('service-fee/1', cores_mcpu),
     ]
+
+
+async def execute_sql_command_in_cli(sql: str, config_file: str):
+    with tempfile.NamedTemporaryFile('w') as script:
+        script.write(sql + '\n')
+        await check_shell(
+            f'''
+mysql --defaults-extra-file={config_file} <{script}
+'''
+        )
 
 
 def create_tables_sql(token):
@@ -76,8 +87,6 @@ CREATE TABLE IF NOT EXISTS `aggregated_job_resources_tmp_{token}` (
 # Note these commands must be run in the CLI and not in aiomysql
 def create_attempt_resources_tmp_after_insert_trigger(token):
     return f'''
-DELIMITER $$
-
 DROP TRIGGER IF EXISTS attempt_resources_tmp_{token}_after_insert $$
 CREATE TRIGGER attempt_resources_tmp_{token}_after_insert AFTER INSERT ON attempt_resources_tmp_{token}
 FOR EACH ROW
@@ -121,16 +130,12 @@ BEGIN
   ON DUPLICATE KEY UPDATE
     `usage` = `usage` + NEW.quantity * msec_diff;
 END $$
-
-DELIMITER ;
 '''
 
 
 # Note these commands must be run in the CLI and not in aiomysql
 def create_attempts_after_update_trigger(token):
     return f'''
-DELIMITER $$
-
 DROP TRIGGER IF EXISTS attempts_after_update $$
 CREATE TRIGGER attempts_after_update AFTER UPDATE ON attempts
 FOR EACH ROW
@@ -197,8 +202,6 @@ BEGIN
     ON DUPLICATE KEY UPDATE `usage` = `usage` + msec_diff * quantity;
   END IF;
 END $$
-
-DELIMITER ;
 '''
 
 
@@ -210,9 +213,56 @@ DROP TABLE IF EXISTS `aggregated_batch_resources_tmp_{token}`;
 DROP TABLE IF EXISTS `aggregated_job_resources_tmp_{token}`;
 '''
 
-def revert_update_attempts_trigger_sql(token):
-    
 
+def revert_triggers_sql(token):
+    return f'''
+DROP TRIGGER IF EXISTS attempt_resources_tmp_{token}_after_insert;
+    
+DELIMITER $$
+
+DROP TRIGGER IF EXISTS attempts_after_update $$
+CREATE TRIGGER attempts_after_update AFTER UPDATE ON attempts
+FOR EACH ROW
+BEGIN
+  DECLARE job_cores_mcpu INT;
+  DECLARE cur_billing_project VARCHAR(100);
+  DECLARE msec_diff BIGINT;
+  DECLARE cur_n_tokens INT;
+  DECLARE rand_token INT;
+
+  SELECT n_tokens INTO cur_n_tokens FROM globals LOCK IN SHARE MODE;
+  SET rand_token = FLOOR(RAND() * cur_n_tokens);
+
+  SELECT cores_mcpu INTO job_cores_mcpu FROM jobs
+  WHERE batch_id = NEW.batch_id AND job_id = NEW.job_id;
+
+  SELECT billing_project INTO cur_billing_project FROM batches WHERE id = NEW.batch_id;
+
+  SET msec_diff = (GREATEST(COALESCE(NEW.end_time - NEW.start_time, 0), 0) -
+                   GREATEST(COALESCE(OLD.end_time - OLD.start_time, 0), 0));
+
+  INSERT INTO aggregated_billing_project_resources (billing_project, resource, token, `usage`)
+  SELECT billing_project, resource, rand_token, msec_diff * quantity
+  FROM attempt_resources
+  JOIN batches ON batches.id = attempt_resources.batch_id
+  WHERE batch_id = NEW.batch_id AND job_id = NEW.job_id AND attempt_id = NEW.attempt_id
+  ON DUPLICATE KEY UPDATE `usage` = `usage` + msec_diff * quantity;
+
+  INSERT INTO aggregated_batch_resources (batch_id, resource, token, `usage`)
+  SELECT batch_id, resource, rand_token, msec_diff * quantity
+  FROM attempt_resources
+  WHERE batch_id = NEW.batch_id AND job_id = NEW.job_id AND attempt_id = NEW.attempt_id
+  ON DUPLICATE KEY UPDATE `usage` = `usage` + msec_diff * quantity;
+
+  INSERT INTO aggregated_job_resources (batch_id, job_id, resource, `usage`)
+  SELECT batch_id, job_id, resource, msec_diff * quantity
+  FROM attempt_resources
+  WHERE batch_id = NEW.batch_id AND job_id = NEW.job_id AND attempt_id = NEW.attempt_id
+  ON DUPLICATE KEY UPDATE `usage` = `usage` + msec_diff * quantity;
+END $$
+
+DELIMITER ;
+'''
 
 
 async def write(db, token, attempt_resources_data, agg_billing_project_data, agg_batch_data, agg_job_data):
@@ -255,20 +305,40 @@ ON DUPLICATE KEY UPDATE `usage` = `usage` + VALUES(`usage`);
 
 async def main():
     db = Database()
-    await db.async_init(config_file='/home/jigold/batch-dump-1-config.json', cursorclass=aiomysql.cursors.SSDictCursor)
+    mysql_config_file = '/home/jigold/mysql-config-batch-dump-1'
+    json_config_file = '/home/jigold/batch-dump-1-config.json'
+    await db.async_init(config_file=json_config_file, cursorclass=aiomysql.cursors.SSDictCursor)
 
     start_time = time.time()
     token = secret_alnum_string(6)
+    print(f'using token {token}')
 
     try:
-        attempt_resources = []
-        agg_billing_project_resources = []
-        agg_batch_resources = []
-        agg_job_resources = []
+        @transaction(db)
+        async def populate(tx):
+            attempt_resources = []
+            agg_billing_project_resources = []
+            agg_batch_resources = []
+            agg_job_resources = []
 
-        n_attempts = 0
+            n_attempts = 0
 
-        async for record in db.select_and_fetchall(
+            await tx.just_execute('LOCK TABLE attempts WRITE;')
+
+            sql = f'''
+{create_tables_sql(token)}
+
+DELIMITER $$
+
+{create_attempt_resources_tmp_after_insert_trigger(token)}
+{create_attempts_after_update_trigger(token)}
+
+DELIMITER ;
+'''
+            await execute_sql_command_in_cli(sql, mysql_config_file)
+
+            # FIXME: Make sure left join keeps all records on the left even if no resources
+            records = tx.select_and_fetchall(
                 '''
 SELECT attempts.batch_id, attempts.job_id, attempts.attempt_id, cores_mcpu, format_version, resource, quantity, start_time, end_time, billing_project
 FROM attempts
@@ -277,68 +347,85 @@ LEFT JOIN attempt_resources ON attempts.batch_id = attempt_resources.batch_id
   AND attempts.attempt_id = attempt_resources.attempt_id
 LEFT JOIN batches ON attempts.batch_id = batches.id
 LEFT JOIN jobs ON attempts.batch_id = jobs.batch_id AND attempts.job_id = jobs.job_id;
-'''):
-            if n_attempts % 1000 == 0:
-                print(f'processed {n_attempts} attempts: elapsed time {time.time() - start_time}s')
-                break
+''')
 
-            n_attempts += 1
+            await tx.just_execute('UNLOCK TABLES;')
 
-            if (len(attempt_resources) >= 1000 or
-                    len(agg_billing_project_resources) >= 1000 or
-                    len(agg_batch_resources) >= 1000 or
-                    len(agg_job_resources) >= 1000
-            ):
-                await write(db, token, attempt_resources, agg_billing_project_resources, agg_batch_resources, agg_job_resources)
-                attempt_resources = []
-                agg_billing_project_resources = []
-                agg_batch_resources = []
-                agg_job_resources = []
-                break
+            async for record in records:
+                if n_attempts % 1000 == 0:
+                    print(f'processed {n_attempts} attempts: elapsed time {time.time() - start_time}s')
+                    break
 
-            if record['format_version'] < 3:
-                resources = get_resources_fmt_version_less_than_3(record['cores_mcpu'])
-            else:
-                resources = [(record['resource'], record['quantity'])]
+                n_attempts += 1
 
-            for resource, quantity in resources:
-                attempt_resources.append((record['batch_id'], record['job_id'], record['attempt_id'], resource, quantity))
+                if (len(attempt_resources) >= 1000 or
+                        len(agg_billing_project_resources) >= 1000 or
+                        len(agg_batch_resources) >= 1000 or
+                        len(agg_job_resources) >= 1000
+                ):
+                    await write(db, token, attempt_resources, agg_billing_project_resources, agg_batch_resources, agg_job_resources)
+                    attempt_resources = []
+                    agg_billing_project_resources = []
+                    agg_batch_resources = []
+                    agg_job_resources = []
+                    break
 
-            if record['start_time'] is not None and record['end_time'] is not None:
-                start_billing_period = datetime.datetime.utcfromtimestamp(record['end_time'] / 1000)
-                start_billing_period = datetime.datetime(start_billing_period.year, start_billing_period.month,
-                                                         start_billing_period.day, tzinfo=datetime.timezone.utc)
-                start_billing_period_utc = start_billing_period.timestamp() * 1000
-
-                end_billing_period = start_billing_period + datetime.timedelta(days=1)
-                end_billing_period_utc = end_billing_period.timestamp() * 1000
-
-                duration_msecs = max(0, record['end_time'] - record['start_time'])
+                if record['format_version'] < 3:
+                    resources = get_resources_fmt_version_less_than_3(record['cores_mcpu'])
+                else:
+                    resources = [(record['resource'], record['quantity'])]
 
                 for resource, quantity in resources:
-                    usage = duration_msecs * quantity
+                    attempt_resources.append((record['batch_id'], record['job_id'], record['attempt_id'], resource, quantity))
 
-                    agg_billing_project_resources.append(
-                        (record['billing_project'], start_billing_period_utc, end_billing_period_utc, resource, 0, usage)
-                    )
-                    agg_batch_resources.append(
-                        (record['batch_id'], start_billing_period_utc, end_billing_period_utc, resource, 0, usage)
-                    )
-                    agg_job_resources.append(
-                        (record['batch_id'], record['job_id'], start_billing_period_utc, end_billing_period_utc, resource, usage)
-                    )
+                if record['start_time'] is not None and record['end_time'] is not None:
+                    start_billing_period = datetime.datetime.utcfromtimestamp(record['end_time'] / 1000)
+                    start_billing_period = datetime.datetime(start_billing_period.year, start_billing_period.month,
+                                                             start_billing_period.day, tzinfo=datetime.timezone.utc)
+                    start_billing_period_utc = start_billing_period.timestamp() * 1000
 
-            n_attempts += 1
+                    end_billing_period = start_billing_period + datetime.timedelta(days=1)
+                    end_billing_period_utc = end_billing_period.timestamp() * 1000
 
-        await write(db, token, attempt_resources, agg_billing_project_resources, agg_batch_resources, agg_job_resources)
-        attempt_resources = []
-        agg_billing_project_resources = []
-        agg_batch_resources = []
-        agg_job_resources = []
+                    duration_msecs = max(0, record['end_time'] - record['start_time'])
 
-        print(f'finished inserting attempts and aggregated resources in {time.time() - start_time}s')
+                    for resource, quantity in resources:
+                        usage = duration_msecs * quantity
+
+                        agg_billing_project_resources.append(
+                            (record['billing_project'], start_billing_period_utc, end_billing_period_utc, resource, 0, usage)
+                        )
+                        agg_batch_resources.append(
+                            (record['batch_id'], start_billing_period_utc, end_billing_period_utc, resource, 0, usage)
+                        )
+                        agg_job_resources.append(
+                            (record['batch_id'], record['job_id'], start_billing_period_utc, end_billing_period_utc, resource, usage)
+                        )
+
+                n_attempts += 1
+
+            await write(db, token, attempt_resources, agg_billing_project_resources, agg_batch_resources, agg_job_resources)
+            attempt_resources = []
+            agg_billing_project_resources = []
+            agg_batch_resources = []
+            agg_job_resources = []
+
+            print(f'finished inserting attempts and aggregated resources in {time.time() - start_time}s')
+
+        await populate()
     except Exception:
-        # revert trigger and cleanup tables
+        print(f'exception occurred after {time.time() - start_time}s; reverting triggers and deleting tables')
+
+        @transaction(db)
+        async def revert(tx):
+            await tx.just_execute('LOCK TABLES attempts WRITE;')
+#         sql = f'''
+# {revert_triggers_sql(token)}
+# {revert_create_tables_sql(token)}
+# '''
+#         await execute_sql_command_in_cli(sql, mysql_config_file)
+            await tx.just_execute('UNLOCK TABLES;')
+        await revert()
         raise
     finally:
         await db.async_close()
