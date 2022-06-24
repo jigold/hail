b712109752c2d52cc6aa88c8cb80bd92a1e2c6bb
Jackie Goldstein <jigold@broadinstitute.org>
2022-06-23 22:20:50 -0400

    insert ignore attempt resources

--- batch/batch/driver/job.py
+++ batch/batch/driver/job.py
@@ -87,9 +87,8 @@ async def add_attempt_resources(db, batch_id, job_id, attempt_id, resources):
 
             await db.execute_many(
                 '''
-INSERT INTO `attempt_resources` (batch_id, job_id, attempt_id, resource, quantity)
-VALUES (%s, %s, %s, %s, %s)
-ON DUPLICATE KEY UPDATE quantity = quantity;
+INSERT IGNORE INTO `attempt_resources` (batch_id, job_id, attempt_id, resource, quantity)
+VALUES (%s, %s, %s, %s, %s);
 ''',
                 resource_args,
                 'add_attempt_resources',
