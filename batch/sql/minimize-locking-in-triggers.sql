7cd8628f00e18c360658ebf06a82626138ebc322
Jackie Goldstein <jigold@broadinstitute.org>
2022-06-24 10:57:05 -0400

    revert remove add_attempt change

--- batch/sql/minimize-locking-in-triggers.sql
+++ batch/sql/minimize-locking-in-triggers.sql
@@ -40,45 +40,45 @@ BEGIN
 END $$
 
 
--- DROP PROCEDURE IF EXISTS add_attempt $$
--- CREATE PROCEDURE add_attempt(
---   IN in_batch_id BIGINT,
---   IN in_job_id INT,
---   IN in_attempt_id VARCHAR(40),
---   IN in_instance_name VARCHAR(100),
---   IN in_cores_mcpu INT,
---   OUT delta_cores_mcpu INT
--- )
--- BEGIN
---   DECLARE cur_instance_state VARCHAR(40);
---   DECLARE dummy_lock INT;
---
---   SET delta_cores_mcpu = IFNULL(delta_cores_mcpu, 0);
---
---   IF in_attempt_id IS NOT NULL THEN
---     SELECT 1 INTO dummy_lock FROM instances_free_cores_mcpu
---     WHERE instances_free_cores_mcpu.name = in_instance_name
---     FOR UPDATE;
---
---     INSERT INTO attempts (batch_id, job_id, attempt_id, instance_name)
---     VALUES (in_batch_id, in_job_id, in_attempt_id, in_instance_name)
---     ON DUPLICATE KEY UPDATE batch_id = batch_id;
---
---     IF ROW_COUNT() != 0 THEN
---       SELECT state INTO cur_instance_state
---       FROM instances
---       WHERE name = in_instance_name
---       LOCK IN SHARE MODE;
---
---       IF cur_instance_state = 'pending' OR cur_instance_state = 'active' THEN
---         UPDATE instances_free_cores_mcpu
---         SET free_cores_mcpu = free_cores_mcpu - in_cores_mcpu
---         WHERE instances_free_cores_mcpu.name = in_instance_name;
---       END IF;
---
---       SET delta_cores_mcpu = -1 * in_cores_mcpu;
---     END IF;
---   END IF;
--- END $$
+DROP PROCEDURE IF EXISTS add_attempt $$
+CREATE PROCEDURE add_attempt(
+  IN in_batch_id BIGINT,
+  IN in_job_id INT,
+  IN in_attempt_id VARCHAR(40),
+  IN in_instance_name VARCHAR(100),
+  IN in_cores_mcpu INT,
+  OUT delta_cores_mcpu INT
+)
+BEGIN
+  DECLARE cur_instance_state VARCHAR(40);
+  DECLARE dummy_lock INT;
+
+  SET delta_cores_mcpu = IFNULL(delta_cores_mcpu, 0);
+
+  IF in_attempt_id IS NOT NULL THEN
+    SELECT 1 INTO dummy_lock FROM instances_free_cores_mcpu
+    WHERE instances_free_cores_mcpu.name = in_instance_name
+    FOR UPDATE;
+
+    INSERT INTO attempts (batch_id, job_id, attempt_id, instance_name)
+    VALUES (in_batch_id, in_job_id, in_attempt_id, in_instance_name)
+    ON DUPLICATE KEY UPDATE batch_id = batch_id;
+
+    IF ROW_COUNT() != 0 THEN
+      SELECT state INTO cur_instance_state
+      FROM instances
+      WHERE name = in_instance_name
+      LOCK IN SHARE MODE;
+
+      IF cur_instance_state = 'pending' OR cur_instance_state = 'active' THEN
+        UPDATE instances_free_cores_mcpu
+        SET free_cores_mcpu = free_cores_mcpu - in_cores_mcpu
+        WHERE instances_free_cores_mcpu.name = in_instance_name;
+      END IF;
+
+      SET delta_cores_mcpu = -1 * in_cores_mcpu;
+    END IF;
+  END IF;
+END $$
 
 DELIMITER ;
