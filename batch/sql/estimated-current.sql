67c60d4c43786771ae125a037062267655bc0f15
Jackie Goldstein <jigold@broadinstitute.org>
2022-08-02 15:14:51 -0400

    more fixes

--- batch/sql/estimated-current.sql
+++ batch/sql/estimated-current.sql
@@ -508,10 +508,6 @@ BEGIN
     WHERE batch_id = NEW.batch_id AND job_id = NEW.job_id AND attempt_id = NEW.attempt_id
     ON DUPLICATE KEY UPDATE `usage` = `usage` + msec_diff * quantity;
   END IF;
-
-  INSERT INTO attempts_aggregated_by_date (batch_id, job_id, attempt_id)
-  VALUES (NEW.batch_id, NEW.job_id, NEW.attempt_id)
-  ON DUPLICATE KEY UPDATE attempt_id = attempt_id;
 END $$
 
 DROP TRIGGER IF EXISTS jobs_after_update $$
