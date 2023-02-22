d3db75518a61a7982dfd81eb9d639030016e5aaf
Jackie Goldstein <jigold@broadinstitute.org>
2023-02-22 13:24:23 -0500

    fix

--- batch/sql/cleanup-dedup-resources.sql
+++ batch/sql/cleanup-dedup-resources.sql
@@ -187,10 +187,10 @@ DROP TRIGGER IF EXISTS aggregated_bp_user_resources_by_date_v2_after_update;
 DROP TRIGGER IF EXISTS aggregated_batch_resources_v2_after_update;
 DROP TRIGGER IF EXISTS aggregated_job_resources_v2_after_update;
 
-ALTER TABLE aggregated_billing_project_user_resources_v2 DROP COLUMN migrated, ALGORITHM=INPLACE, LOCK=NONE;
-ALTER TABLE aggregated_billing_project_user_resources_by_date_v2 DROP COLUMN migrated, ALGORITHM=INPLACE, LOCK=NONE;
-ALTER TABLE aggregated_batch_resources_v2 DROP COLUMN migrated, ALGORITHM=INPLACE, LOCK=NONE;
-ALTER TABLE aggregated_job_resources_v2 DROP COLUMN migrated, ALGORITHM=INPLACE, LOCK=NONE;
+-- ALTER TABLE aggregated_billing_project_user_resources_v2 DROP COLUMN migrated, ALGORITHM=INPLACE, LOCK=NONE;
+-- ALTER TABLE aggregated_billing_project_user_resources_by_date_v2 DROP COLUMN migrated, ALGORITHM=INPLACE, LOCK=NONE;
+-- ALTER TABLE aggregated_batch_resources_v2 DROP COLUMN migrated, ALGORITHM=INPLACE, LOCK=NONE;
+-- ALTER TABLE aggregated_job_resources_v2 DROP COLUMN migrated, ALGORITHM=INPLACE, LOCK=NONE;
 
 DROP TRIGGER IF EXISTS resources_before_insert;
 
@@ -199,9 +199,9 @@ DROP TRIGGER IF EXISTS resources_before_insert;
 -- has to stay despite being the original non-deduped resource id column. It will be deleted once the trigger that uses the
 -- deduped_resource_id is dropped
 ALTER TABLE attempt_resources DROP PRIMARY KEY,
+                              MODIFY COLUMN deduped_resource_id INT NOT NULL,
                               RENAME COLUMN resource_id TO deduped_resource_id,
                               RENAME COLUMN deduped_resource_id TO resource_id,
-                              MODIFY COLUMN resource_id INT NOT NULL,
                               ADD PRIMARY KEY (`batch_id`, `job_id`, `attempt_id`, `resource_id`),
                               ALGORITHM=INPLACE, LOCK=NONE;
 
