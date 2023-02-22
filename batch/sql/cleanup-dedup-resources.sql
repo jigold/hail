81cf621e565ee480e555f5756149e9c3f9780a61
Jackie Goldstein <jigold@broadinstitute.org>
2023-02-22 13:45:36 -0500

    fix

--- batch/sql/cleanup-dedup-resources.sql
+++ batch/sql/cleanup-dedup-resources.sql
@@ -208,10 +208,10 @@ ALTER TABLE attempt_resources RENAME COLUMN resource_id TO deduped_resource_id,
 --                               ADD PRIMARY KEY (`batch_id`, `job_id`, `attempt_id`, `resource_id`),
 --                               ALGORITHM=INPLACE, LOCK=NONE;
 
-SET foreign_key_checks = 0;
-ALTER TABLE attempt_resources ADD FOREIGN KEY (`resource_id`) REFERENCES resources(`resource_id`) ON DELETE CASCADE, ALGORITHM=INPLACE;
-SET foreign_key_checks = 1;
+-- SET foreign_key_checks = 0;
+-- ALTER TABLE attempt_resources ADD FOREIGN KEY (`resource_id`) REFERENCES resources(`resource_id`) ON DELETE CASCADE, ALGORITHM=INPLACE;
+-- SET foreign_key_checks = 1;
 
 DROP TRIGGER IF EXISTS attempt_resources_before_insert;
 
-ALTER TABLE attempt_resources DROP COLUMN deduped_resource_id, ALGORITHM=INPLACE, LOCK=NONE;
+-- ALTER TABLE attempt_resources DROP COLUMN deduped_resource_id, ALGORITHM=INPLACE, LOCK=NONE;
