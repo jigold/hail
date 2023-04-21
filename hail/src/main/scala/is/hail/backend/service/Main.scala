c88fa0ab4a1693cf9fec882a0683a5738e4302ab
Jackie Goldstein <jigold@broadinstitute.org>
2023-04-21 13:30:09 -0400

    Revert "testing"
    This reverts commit 4d10ae73f932479174f40fcb2529b48972a2160a.

--- hail/src/main/scala/is/hail/backend/service/Main.scala
+++ hail/src/main/scala/is/hail/backend/service/Main.scala
@@ -1,21 +1,27 @@
 package is.hail.backend.service
 
 import is.hail.HailContext
-import org.apache.log4j.{FileAppender, Level, LogManager, PatternLayout}
+import org.apache.log4j.{LogManager, PropertyConfigurator}
 
+import java.util.Properties
 
 object Main {
   val WORKER = "worker"
   val DRIVER = "driver"
 
   def configureLogging(logFile: String): Unit = {
-    val logger = LogManager.getRootLogger()
-    logger.removeAllAppenders()
-    val fa = new FileAppender()
-    fa.setFile(logFile)
-    fa.setLayout(new PatternLayout(HailContext.logFormat))
-    fa.setThreshold(Level.INFO)
-    logger.addAppender(fa)
+    val logProps = new Properties()
+
+    logProps.put("log4j.rootLogger", "INFO, logfile")
+    logProps.put("log4j.appender.logfile", "org.apache.log4j.FileAppender")
+    logProps.put("log4j.appender.logfile.append", true.toString)
+    logProps.put("log4j.appender.logfile.file", logFile)
+    logProps.put("log4j.appender.logfile.threshold", "INFO")
+    logProps.put("log4j.appender.logfile.layout", "org.apache.log4j.PatternLayout")
+    logProps.put("log4j.appender.logfile.layout.ConversionPattern", HailContext.logFormat)
+
+    LogManager.resetConfiguration()
+    PropertyConfigurator.configure(logProps)
   }
 
   def main(argv: Array[String]): Unit = {
