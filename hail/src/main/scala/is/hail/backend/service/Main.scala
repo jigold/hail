4d10ae73f932479174f40fcb2529b48972a2160a
Jackie Goldstein <jigold@broadinstitute.org>
2023-04-21 11:55:19 -0400

    testing

--- hail/src/main/scala/is/hail/backend/service/Main.scala
+++ hail/src/main/scala/is/hail/backend/service/Main.scala
@@ -1,27 +1,21 @@
 package is.hail.backend.service
 
 import is.hail.HailContext
-import org.apache.log4j.{LogManager, PropertyConfigurator}
+import org.apache.log4j.{FileAppender, Level, LogManager, PatternLayout}
 
-import java.util.Properties
 
 object Main {
   val WORKER = "worker"
   val DRIVER = "driver"
 
   def configureLogging(logFile: String): Unit = {
-    val logProps = new Properties()
-
-    logProps.put("log4j.rootLogger", "INFO, logfile")
-    logProps.put("log4j.appender.logfile", "org.apache.log4j.FileAppender")
-    logProps.put("log4j.appender.logfile.append", true.toString)
-    logProps.put("log4j.appender.logfile.file", logFile)
-    logProps.put("log4j.appender.logfile.threshold", "INFO")
-    logProps.put("log4j.appender.logfile.layout", "org.apache.log4j.PatternLayout")
-    logProps.put("log4j.appender.logfile.layout.ConversionPattern", HailContext.logFormat)
-
-    LogManager.resetConfiguration()
-    PropertyConfigurator.configure(logProps)
+    val logger = LogManager.getRootLogger()
+    logger.removeAllAppenders()
+    val fa = new FileAppender()
+    fa.setFile(logFile)
+    fa.setLayout(new PatternLayout(HailContext.logFormat))
+    fa.setThreshold(Level.INFO)
+    logger.addAppender(fa)
   }
 
   def main(argv: Array[String]): Unit = {
