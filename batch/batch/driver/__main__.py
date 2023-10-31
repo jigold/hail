7bb97eb4bffba4c1d25e44eaccd64bc98800ef2c
Jackie Goldstein <jigold@broadinstitute.org>
2023-10-31 15:26:10 -0400

    start uvloop properly

--- batch/batch/driver/__main__.py
+++ batch/batch/driver/__main__.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 
 from hailtop.hail_logging import configure_logging
@@ -9,8 +10,11 @@ import sys  # noqa: E402 pylint: disable=wrong-import-position
 import traceback  # noqa: E402 pylint: disable=wrong-import-position
 
 import aiohttp  # noqa: E402 pylint: disable=wrong-import-position
+import uvloop
 
 from .main import run  # noqa: E402 pylint: disable=wrong-import-position
+
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 oldinit = aiohttp.ClientSession.__init__  # type: ignore
 
